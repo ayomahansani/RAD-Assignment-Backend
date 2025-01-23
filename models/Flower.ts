a32a2bd045c654a255de56615aa43cdf6afd3650
ayomahansani <ayohansi17@gmail.com>
2025-01-23 20:47:54 +0530

    create Flower.ts

---
+++ models/Flower.ts
@@ -0,0 +1,8 @@
+export default class Flower {
+    flower_code!: number;
+    flower_name!: string;
+    flower_image!: string;
+    flower_quality!: string;
+    flower_colour!: string;
+    seller!: string;
+}
