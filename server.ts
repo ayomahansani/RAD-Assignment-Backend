9f36b51d800eb73d27749b0057cf2584d0f9eef9
ayomahansani <ayohansi17@gmail.com>
2025-01-31 17:15:41 +0530

    create auth routes

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use('/', (req, res, next)=>{
     res.header("Access-Control-Allow-Origin", "*");
@@ -13,14 +13,16 @@ app.use('/', (req, res, next)=>{
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type');
 
     next();
-})
+});
 
-app.use('/flower')
+console.log("Loaded SECRET_KEY:", process.env.SECRET_KEY);
+
+app.use('/auth', authRoutes);
 
 app.listen(3000, (err =>{
     console.log("Server running on port 3000");
-}))
+}));
 
 app.use('/', (req, res, next)=>{
     res.status(200).send('Not Found');
-})
+});
