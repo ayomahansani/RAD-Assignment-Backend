f89d8c5fb374e160c949b0c8fb838a52a9ba9fc4
ayomahansani <ayohansi17@gmail.com>
2025-02-09 22:23:56 +0530

    create Customer class

---
+++ models/Customer.ts
@@ -0,0 +1,9 @@
+export interface Customer {
+    customer_id?: number;
+    customer_firstName: string
+    customer_lastName: string
+    customer_phone: string
+    customer_email: string
+    customer_address: string
+    gender: string
+}
